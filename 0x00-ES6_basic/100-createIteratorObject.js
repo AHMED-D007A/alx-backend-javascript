5c239b797396bfdb0aa82c4aacd01343dda63e69
AHMED-D007A <ahmed.mamdouh8840@gmail.com>
2024-09-30 17:17:53 +0000

    eslint

--- 0x00-ES6_basic/100-createIteratorObject.js
+++
@@ -1,9 +0,0 @@
-export default function createIteratorObject(report) {
-  const returnedValues = [];
-  const iter = (Object.values(report.allEmployees)[Symbol.iterator]());
-  for (const element of iter) {
-    returnedValues.push(...element);
-  }
-
-  return returnedValues;
-}
